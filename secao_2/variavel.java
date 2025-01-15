3791d423c09e6ee7e5861ccc6d7a07fbdd0afa60
hebertaragao <hebert.aragao@outlook.com>
2025-01-15 11:36:44 -0300

    Created section 2 on variables

---
+++ secao_2/variavel.java
@@ -0,0 +1,14 @@
+package secao_2;
+
+public class variavel {
+
+    public static void main(String[] args) {
+        
+        // 1 - O que são variáveis?
+        // tipo -> nome -> atribuir um valor;
+        String nome = "Hebert";
+
+        // nome => "Hebert"
+        System.out.println(nome);
+    }
+}
